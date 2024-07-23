a9039b31c1134af7f80f3527676f86bd14043b66
Filip Jorissen <filip.jorissen@gmail.com>
2024-07-23 14:22:27 +0200

    merged ibpsa sha f5b23786371a189e7f6478d8bdb88e497277d3c3

--- IDEAS/RunUnitTests.py
+++ IDEAS/RunUnitTests.py
@@ -29,7 +29,7 @@ import buildingspy.development.regressiontest as rt
 tester=r.Tester()
 
 """ number of parallel processes started."""
-tester.setSinglePackage("IBPSA.Airflow")
+tester.setSinglePackage("IBPSA.Utilities.IO.Files")
 #tester.setNumberOfThreads(4)
 
 """ Html validation on/off"""
